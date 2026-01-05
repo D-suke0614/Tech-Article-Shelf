24dd86fe65f4c8b8fc4c84c28f777b83729024ee
Daisuke <d.suke.kida@gmail.com>
2026-01-05 23:26:57 +0900

    add: jest

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Next.jsアプリのパスを指定（next.config.jsがあるディレクトリ）
+  dir: './',
+})
+
+// Jestのカスタム設定
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/?(*.)+(spec|test).[jt]s?(x)',
+  ],
+}
+
+// createJestConfigは非同期なので、exportする
+module.exports = createJestConfig(customJestConfig)
---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+// Jestのセットアップファイル
+// テストの前に実行される設定をここに記述できます
+
+import '@testing-library/jest-dom'
